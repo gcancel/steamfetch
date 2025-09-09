d8339567128d1f4be76722e6b70b90318a0aefbd
gcancel <gmartin9892@gmail.com>
2025-09-08 22:52:29 -0500

    added ANSI coloring to terminal, steam logo, and other styling.

--- handleSteamFetch.go
+++ handleSteamFetch.go
@@ -26,28 +26,49 @@ func handleSteamFetch(s *state, cmd command) error {
 	totalGameTimeAllMonths := totalGameTimeAllDays / 30
 	totalGameTimeAllYears := fmt.Sprintf("%.1f year/s", float64(totalGameTimeAllMonths)/12)
 	// Print out
-	fmt.Printf("Your steam id: %v\n", s.steamID)
-	fmt.Printf("Total number of games: %v\n", gameCount)
-	fmt.Printf("Total steam gameplay time:\n%v minutes\n->| %v hours\n->| %v days\n->| %v month/s\n->| %v...\n",
+	arrow := setANSIText("->|", Blue)
+	logo := `
+	⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+	⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠋⠉⠉⠉⠉⠻⣿⣿⣿⣿
+	⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⢀⣴⣶⣶⣄⠀⠈⢻⣿⣿
+	⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⢸⣿⣿⣿⣿⡇⠀⢸⣿⣿
+	⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⠀⠀⠈⠻⠿⠿⠛⠁⠀⣸⣿⣿
+	⠿⣿⣿⣿⣿⡿⠟⠛⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣼⣿⣿⣿
+	⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣾⣿⣿⣿⣿⣿⣿
+	⣶⣶⣤⡀⠀⠀⣴⣿⣦⠀⠀⠀⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿
+	⣿⣿⣿⣧⡀⠀⠙⠿⠋⠀⠀⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+	⣿⣿⣿⣿⣷⣤⣀⣀⣀⣀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+	⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+`
+	fmt.Println(setANSIText(logo, Blue))
+	fmt.Printf("%v %v\n", setANSIText("Your steam id:", Yellow), s.steamID)
+	fmt.Printf("%v %v\n", setANSIText("Total number of games:", Yellow), gameCount)
+	fmt.Printf("%v\n%s%v minutes\n%s %v hours\n%s %v days\n%s %v month/s\n%s %v...\n",
+		setANSIText("Total steam gameplay time:", Yellow),
+		arrow,
 		totalGameTimeForeverMins,
+		arrow,
 		totalGameTimeAllHours,
+		arrow,
 		totalGameTimeAllDays,
+		arrow,
 		totalGameTimeAllMonths,
+		arrow,
 		totalGameTimeAllYears,
 	)
 
-	fmt.Printf("Total steam gameplay time (2 week period): %v minutes\n", int(totalGameTime2Weeks.Float64))
-	fmt.Println("Most played games:")
+	fmt.Printf("%v %v minutes\n", setANSIText("Total steam gameplay time (2 week):", Yellow), int(totalGameTime2Weeks.Float64))
+	fmt.Println(setANSIText("Most played games:", Yellow))
+	fmt.Println(setANSIText("----------------------------------", Blue))
 	limit := 5
 	mostPlayedGames, err := s.dbQueries.GetTopPlayedGames(context.Background(), int64(limit))
 	if err != nil {
 		log.Fatal(err)
 	}
 	for _, game := range mostPlayedGames {
-		fmt.Printf("->|%v: %v mins\n", game.Name, game.PlaytimeForever)
+		fmt.Printf("%s%v: %v mins\n", arrow, game.Name, game.PlaytimeForever)
 	}
-	// create query for most played games
-	fmt.Printf("...")
+	fmt.Println(setANSIText("----------------------------------", Blue))
 
 	return nil
 }
--- util.go
+++ util.go
@@ -97,3 +97,20 @@ func initDatabase() (*sql.DB, error) {
 	_, err := os.Stat(filename)
 	return !errors.Is(err, os.ErrNotExist)
 } */
+
+// ANSI Escape Color Codes
+const (
+	Reset   = "\033[0m"
+	Red     = "\033[31m"
+	Green   = "\033[32m"
+	Yellow  = "\033[33m"
+	Blue    = "\033[34m"
+	Magenta = "\033[35m"
+	Cyan    = "\033[36m"
+	Gray    = "\033[37m"
+	White   = "\033[97m"
+)
+
+func setANSIText(s, code string) string {
+	return code + s + Reset
+}
